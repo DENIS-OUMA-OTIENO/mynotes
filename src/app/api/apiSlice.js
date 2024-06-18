844395305d2368949d2a8d3f3cbc945bbf3a5961
DENIS_OUMA_OTIENO <48428763+DENIS-OUMA-OTIENO@users.noreply.github.com>
2024-06-18 16:00:19 +0300

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://notes-api.onrender.com',
+    baseUrl: 'https://mynotes-api-glju.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
